c8d6e3e0ac59f669e342b605491ea63eba809727
Mohammadfazal-sayed <66768407+Mohammadfazal-sayed@users.noreply.github.com>
2020-08-25 17:00:42 +0800

    Update tmp.sh

--- tmp.sh
+++ tmp.sh
@@ -1,10 +1,16 @@
-ssh -i ubuntu@172.31.51.50 '
+aws ec2 run-instances \
+    --image-id ami-0bcc094591f354be2 \
+    --instance-type t2.micro \
+    --subnet-id subnet-7f879441 \
+    --security-group-ids sg-07142950944cc283c \
+    --associate-public-ip-address \
+    --key-name sbk \
+    --region us-east-1
+    
+    
 ip=`aws ec2 describe-instances --region us-east-1 --query "Reservations[*].Instances[*].PublicIpAddress" --output=text | tail -1`
 
+ssh -i "/home/ubuntu/sbk.pem" ubuntu@$ip '
 sudo su -
 sudo hostname Nagiosslave4
-sudo echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDMptrDTq6LO1CVEcdRz283UxigSp3sFXILB9KBcI2jiG9ILM7ZaC5xVhk1TKsimJCBxqci7iLcur5cMiiyyUVYfW6qzoydkkjABtW+n4GizGOERFCamiFKfOxSq9ZyQngXqKoexDiCKsmX8Jgo25ET32cXhu20f3iEHXcFsuY9CC4QqXCuSj579BPZqqqeQPJEZwjXTutkjg275IW49xz+GlMAu46nu3PuT3lAhUd5NohxmYSnOQ2PD3dQco36qJrKW9w0Izvbo4PhAPvOMH7lHhxS+bXMag4ha2yI2MfvM1pcbhSp4rUlvj5zTdXj/u9xbKdRGagObBbK/SLwoT/p jenkins@JenkinsServer
-" >> /home/ubuntu/.ssh/authorized_keys
-sudo echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCirDOHKvfoIDlEiM8fZhrEO/feep05vs198gJUI3Va7ggEXO8zyQaFnAbDcfBL0aAOH5nrpEfSQE1nikjHVE+ytUtRjElqwtGtuTRdP65zkRZQ9GAvNhqZW8mBB5QXhLbY96PVf4K62mc2tQqm1B6duCvpqDnmui8ImDHAeVM/Tfg2pmwkTn90BdFSsXXhUhXTRCIbBt/sn0MdpKrMyh62vsMB1F7mQzi+mKvfzyYEWuDXTFwVZ6z1QNs9jPyX01joj9eWX+KCFO6m//lzzOCsgTe0eR7Bd8tjoEo9PiF50aGG2WV6mBeUH/20qpNA8OwP6rsb/+Gne66EoUlaI8cT ubuntu@NagiosMaster
-" >> /home/ubuntu/.ssh/authorized_keys
 '
