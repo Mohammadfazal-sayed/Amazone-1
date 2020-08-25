8b7c022ec03cff83b71d71a6729a52273a77f856
Mohammadfazal-sayed <66768407+Mohammadfazal-sayed@users.noreply.github.com>
2020-08-25 16:34:14 +0800

    Update aws3.sh

--- aws3.sh
+++ aws3.sh
@@ -1,11 +1,4 @@
-aws ec2 run-instances \
-    --image-id ami-0bcc094591f354be2 \
-    --instance-type t2.micro \
-    --subnet-id subnet-7f879441 \
-    --security-group-ids sg-07142950944cc283c \
-    --associate-public-ip-address \
-    --key-name sbk \
-    --region us-east-1
+
     
     
 ip=`aws ec2 describe-instances --region us-east-1 --query "Reservations[*].Instances[*].PublicIpAddress" --output=text | tail -1`
