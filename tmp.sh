ab341a5396cb56b0c825f11884d44a0410cb3b32
Mohammadfazal-sayed <66768407+Mohammadfazal-sayed@users.noreply.github.com>
2020-08-28 17:06:20 +0530

    Update tmp.sh

--- tmp.sh
+++ tmp.sh
@@ -14,14 +14,14 @@ sudo chmod 777 -R /usr/local/nagios/
 sudo echo " 
 define host {
         use                          linux-server
-        host_name                    Nagios4
+        host_name                    Nagios11
         alias                        Ubuntu Host
         address                      $ip
         register                     1
 
 }
 define service {
-      host_name                       Nagios4
+      host_name                       Nagios11
       service_description             PING
       check_command                   check_ping!100.0,20%!500.0,60%
       max_check_attempts              2
@@ -36,7 +36,7 @@ define service {
       register                        1
 }
 define service {
-      host_name                       Nagios4
+      host_name                       Nagios11
       service_description             Check Users
       check_command           check_local_users!20!50
       max_check_attempts              2
@@ -66,7 +66,7 @@ define service {
       register                        1
 }
 define service {
-      host_name                       Nagios4
+      host_name                       Nagios11
       service_description             Check SSH
       check_command                   check_ssh
       max_check_attempts              2
@@ -81,7 +81,7 @@ define service {
       register                        1
 }
 define service {
-      host_name                       Nagios4
+      host_name                       Nagios11
       service_description             Total Process
       check_command                   check_local_procs!250!400!RSZDT
       max_check_attempts              2
